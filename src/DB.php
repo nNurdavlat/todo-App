713d86d01ed4e87d42364a9d6412e49f8604d411
Mexriddin <mekkinuriddinov@gmail.com>
2024-12-10 21:12:07 +0500

    Minor: register added

--- src/DB.php
+++ src/DB.php
@@ -1,10 +1,11 @@
 <?php
+namespace App;
 class DB
 {
     public $conn;
 
     public function __construct()
     {
-        $this->conn = new PDO("mysql:host=" . $_ENV['DB_HOST'] . ";dbname=".$_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+        $this->conn = new \PDO("mysql:host=" . $_ENV['DB_HOST'] . ";dbname=".$_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
     }
 }
